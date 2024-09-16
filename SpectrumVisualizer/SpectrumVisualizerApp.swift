95c325e72aef3987ffa6c51d630b650c8880fa80
Min <lebonthe@gmail.com>
2024-09-16 17:03:22 +0800

    Initial Commit

---
+++ SpectrumVisualizer/SpectrumVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpectrumVisualizerApp.swift
+//  SpectrumVisualizer
+//
+//  Created by S485856 on 2024/9/16.
+//
+
+import SwiftUI
+
+@main
+struct SpectrumVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
